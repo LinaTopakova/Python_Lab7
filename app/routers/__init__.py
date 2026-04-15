529e1c2733388555f56c5d4f5ef563e52319be04
Udynix <129510560+LinaTopakova@users.noreply.github.com>
2026-04-15 23:36:51 +0700

    init: базовое FastAPI приложение

---
+++ app/routers/__init__.py
@@ -0,0 +1,4 @@
+from .tasks import tasks_router
+from .websocket import websocket_router
+
+__all__ = ["tasks_router", "websocket_router"]
---
+++ app/routers/tasks.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+tasks_router = APIRouter(prefix="/tasks", tags=["tasks"])
+
+@tasks_router.get("/")
+async def get_tasks():
+    return {"tasks": []}
